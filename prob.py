d22e59391fbac6dd5ad3a5d1d39918025066fd2d
abishaalbert14 <35557981+abishaalbert14@users.noreply.github.com>
2018-01-27 12:02:11 +0530

    Update prob.py

--- prob.py
+++ prob.py
@@ -1,5 +1,6 @@
-num = int(input('Enter any number: '))
-if num % 2 == 0:
-    print(num, "is EVEN")
+num=int(input ("enter any number:"))
+if num%2 == 0:
+ print("num is even")
 else:
-    print(num, "is ODD")
+    print("num is odd")
+
