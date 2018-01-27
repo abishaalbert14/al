b275d0284eccc5bcdb0b93ffdbaa4d1426f37442
abishaalbert14 <35557981+abishaalbert14@users.noreply.github.com>
2018-01-27 12:38:20 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,7 @@
+alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+char = input('enter the letter:')
+if char in alphabets:
+  print('alphabets')
+
+else:
+  print('not a alphabet')
