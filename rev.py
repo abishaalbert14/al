7a3ea23699b8b5d8bc5fd04d04a3fb952b1fa9e0
abishaalbert14 <35557981+abishaalbert14@users.noreply.github.com>
2018-02-05 11:43:18 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,10 +1,16 @@
-string=input("enter any string:")
-if string='x':
-break
-else:
-newstr=string
-vowels=('a','e','i','o','u')
-for x in string.lower():
-if x in vowels:
-newstr=newstr.replace(x,"")
-print(newstr)
+while True:
+	print("Enter 'x' for exit.")
+	string = input("Enter any string: ")
+	if string == 'x':
+		break
+	else:
+		newstr = string
+		print("\nRemoving vowels ")
+		vowels = ('a', 'e', 'i', 'o', 'u')
+		for x in string.lower():
+			if x in vowels:
+				newstr = newstr.replace(x,"")
+		print("New string ")
+		print(newstr,"\n")
+		
+		
