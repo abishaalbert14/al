88575de5fc87bb561d3da7eee87555e44be9ec15
abishaalbert14 <35557981+abishaalbert14@users.noreply.github.com>
2018-02-05 12:08:00 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,10 @@
+def swap(a,b):
+    tmp=a
+    a=b
+    b=tmp
+    print('after swap:n1=",a,"n2=",b)
+    return
+n1=input("enter first number")
+n2=input("enter second number")
+print('before swap:n1=","n2=",n2)
+      swap(n1,n2)
